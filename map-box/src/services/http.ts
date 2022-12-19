ac45edb692d999d4bf5d6dbe091361a5666c8e64
mahmoud3ali <mahmoud.ali.fcis@gmail.com>
2022-12-19 06:15:52 +0200

    feat(client): add http service

---
+++ map-box/src/services/http.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosInstance } from "axios";
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: "http://127.0.0.1:7123/",
+});
+
+const init = (callback: (axios: AxiosInstance) => void) => {
+  callback(axiosInstance);
+};
+
+const setJwt = (token?: string) => {
+  if (token) {
+    axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete axiosInstance.defaults.headers.common["Authorization"];
+  }
+};
+
+const defaultHandleForNonHttpError = (error: unknown) => {
+  console.error(error);
+};
+
+const http = {
+  init,
+  setJwt,
+  defaultHandleForNonHttpError,
+  isHttpError: axios.isAxiosError,
+  request: axiosInstance.request,
+  get: axiosInstance.get,
+  post: axiosInstance.post,
+  put: axiosInstance.put,
+  patch: axiosInstance.patch,
+  delete: axiosInstance.delete,
+};
+
+export default http;
