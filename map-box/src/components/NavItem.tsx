0b34b95cf8e5e9c4d09a4afed6e713ecea42adb9
mahmoud3ali <mahmoud.ali.fcis@gmail.com>
2022-12-19 06:14:28 +0200

    feat(client): add NavItem component

---
+++ map-box/src/components/NavItem.tsx
@@ -0,0 +1,25 @@
+import { Link as MaterialLink } from "@mui/material";
+import { Link as RouterLink, useMatch } from "react-router-dom";
+
+type Props = {
+  path: string;
+  name: string;
+};
+export function NavItem({ path, name }: Props) {
+  const isMatch = useMatch(path);
+  return (
+    <MaterialLink
+      component={RouterLink}
+      to={path}
+      color="inherit"
+      underline="none"
+      sx={{
+        mr: 2,
+        fontWeight: isMatch ? "bold" : "normal",
+        textDecoration: isMatch ? "underline" : "none",
+      }}
+    >
+      {name}
+    </MaterialLink>
+  );
+}
